761d2cf4c3deda659f9c0b95f01570ae4361cfb9
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-09-18 17:31:51 +0200

    feat: envs validation with joi

---
+++ 02-Products-App/products-ms/src/config/envs.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface envVars {
+  port: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+export const envs: envVars = value;
