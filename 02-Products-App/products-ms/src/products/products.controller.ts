8a7452cd1baf42d04249971647b2e65582287e86
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-09-18 18:44:15 +0200

    feat: update and delete product with validations

--- 02-Products-App/products-ms/src/products/products.controller.ts
+++ 02-Products-App/products-ms/src/products/products.controller.ts
@@ -35,12 +35,15 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: string) {
     return this.productsService.remove(+id);
   }
 }
